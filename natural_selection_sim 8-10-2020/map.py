afe1682a24a8a0405c648896694eef4703e74f66
phletic <59368044+phletic@users.noreply.github.com>
2020-10-08 17:06:53 +0800

    Update map.py

--- natural_selection_sim 8-10-2020/map.py
+++ natural_selection_sim 8-10-2020/map.py
@@ -3,9 +3,16 @@ from food import plant
 from vectorMath import Vector
 import time
 
+'''
+driver program. 
+'''
+
 map = [prey(Vector(0, 0), 10), prey(Vector(1, 0), 10), plant(Vector(2, 0), 5)]
+
+#this starts up the organisms. For now, it only assigns its type (str). But I think it will be very useful later when the program gets moew advanced
 for i in map:
     i.start()
+    
 if __name__ == '__main__':
     while True:
         int = input("")
