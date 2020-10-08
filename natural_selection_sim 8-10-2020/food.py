4664ed93594582da7e83d3e4b6dc440c2627a59f
phletic <59368044+phletic@users.noreply.github.com>
2020-10-08 17:12:25 +0800

    Update food.py

--- natural_selection_sim 8-10-2020/food.py
+++ natural_selection_sim 8-10-2020/food.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
 from vectorMath import Vector
 
-
+'''
+This program is for the plants
+'''
+#parent
 class food(ABC):
     def __init__(self, pos, energy):
         self.pos = pos
@@ -15,7 +18,7 @@ class food(ABC):
     def start(self):
         pass
 
-
+#child. like animal, I wanna have some actual plants stemming out of this class
 class plant(food):
     def start(self):
         self.type = 'plant'
